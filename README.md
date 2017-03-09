1df0754a6670681b8acc7f9b687d02e239840b85
Andres Vizcaino <andruvizcaino1989@gmail.com>
2017-03-09 17:37:24 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# KV
+
+The application works as a distributed key-value store. Organizing key-value pairs into buckets and distribute those buckets across multiple nodes. It has a simple client that allows us to connect to any of those nodes and send requests using **OTP**, **Mix** and **ExUnit**
---
+++ test/bucket_test.exs
@@ -0,0 +1,36 @@
+defmodule KV.BucketTest do
+  @doc """
+  use ExUnit.Case, async: true
+
+  setup do
+    {:ok, bucket} = KV.Bucket.start_link
+    {:ok, bucket: bucket}
+  end
+
+  test "stores values by key", %{bucket: bucket} do
+    assert KV.Bucket.get(bucket, "milk") == nil
+
+    KV.Bucket.put(bucket, "milk", 3)
+    assert KV.Bucket.get(bucket, "milk") == 3
+  end
+  """
+
+  use ExUnit.Case, async: true
+
+  test "stores values by key" do
+    {:ok, bucket} = KV.Bucket.start_link
+    assert KV.Bucket.get(bucket, "milk") == nil
+
+    KV.Bucket.put(bucket, "milk", 3)
+    assert KV.Bucket.get(bucket, "milk") == 3
+  end
+
+  test "deletes and returns the current value by a key " do
+    {:ok, bucket} = KV.Bucket.start_link
+    KV.Bucket.put(bucket, "milk", 3)
+    assert KV.Bucket.get(bucket, "milk") == 3
+    
+    KV.Bucket.delete(bucket, "milk")
+    assert KV.Bucket.get(bucket, "milk") == nil
+  end
+end
